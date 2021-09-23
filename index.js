ce6e5aed346dff0499d32817482e13430af35551
Filip Tammergård <filip@tammergard.se>
2021-09-23 15:03:46 +0200

    feat: set all config rules explicitly

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// defaults are commented out
+// defaults in comments
 
 module.exports = {
-  // printWidth: 80,
-  // tabWidth: 2,
-  // useTabs: false,
-  semi: false,
-  // singleQuote: false,
-  // quoteProps: "as-needed",
-  // jsxSingleQuote: false,
-  trailingComma: "all",
-  // bracketSpacing: true,
-  // jsxBracketSameLine: false,
-  // arrowParens: "always",
-  // rangeStart: 0,
-  // rangeEnd: Infinity,
-  // requirePragma: false,
-  // insertPragma: false,
-  // proseWrap: "preserve",
-  // htmlWhitespaceSensitivity: "css",
-  // endOfLine: "lf",
-  // embeddedLanguageFormatting: "auto",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false, // true
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all", // "es5"
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "always", // "preserve"
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
 }
